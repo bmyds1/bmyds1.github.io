9a0cd1f4c07a9c3d7e17ad93648a8126ad2d9b2e
bmyds1 <78897051+bmyds1@users.noreply.github.com>
2021-02-22 22:59:38 +0900

    最終更新日の自動更新、メタタグ調整

--- pj_sekai/musics/js/main.js
+++ pj_sekai/musics/js/main.js
@@ -1,5 +1,13 @@
 window.addEventListener("load", () => {
-    document.getElementById("h1").innerText = document.title;
+    try {
+        let d = new Date(document.lastModified);
+        let ds = d.toLocaleDateString();
+        let str = " (" + ds + "更新)";
+        document.getElementById("lastModified").innerText = str;
+        document.title += str;
+    } catch (e) {
+        console.log(e);
+    }
     initOptions();
     initMusicsTable();
     document.getElementById("form").addEventListener("change", initMusicsTable);
