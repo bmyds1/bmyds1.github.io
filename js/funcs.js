2cc1f5a65768cfe7015b99fb13aa2b2b5680a596
bmyds1 <78897051+bmyds1@users.noreply.github.com>
2021-02-24 11:54:14 +0900

    神高掛け合い追加、フッター自動生成

---
+++ js/funcs.js
@@ -0,0 +1,60 @@
+const resetElement = (id) => {
+    let d = document.getElementById(id);
+    let newD = d.cloneNode(false);
+    d.parentNode.replaceChild(newD, d);
+    d.id = id;
+}
+const getTd = (text, className = null) => {
+    return getElm("td", text, className);
+}
+const getTh = (text, className = null) => {
+    return getElm("th", text, className);
+}
+const getSpan = (text, className = null) => {
+    return getElm("span", text, className);
+}
+const getP = (text, className = null) => {
+    return getElm("p", text, className);
+}
+const getLi = (text, className = null) => {
+    return getElm("li", text, className);
+}
+const getBr = () => {
+    return document.createElement("br");
+}
+const getElm = (elm, text, className = null) => {
+    let d = document.createElement(elm);
+    d.innerText = text;
+    if (className != null) {
+        d.className = className;
+    }
+    return d;
+}
+const getCheckboxLabel = (name, value, text, isChecked = false) => {
+    let label = document.createElement("label");
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.name = name;
+    checkbox.value = value;
+    checkbox.checked = isChecked;
+    label.appendChild(checkbox);
+    let spanText = document.createElement("span");
+    spanText.innerText = text;
+    spanText.style.userSelect = "none";
+    label.appendChild(spanText);
+    return label;
+}
+const concatElms = (elms, joint) => {
+    let span = getSpan("");
+    let flag = false;
+    for (let e of elms) {
+        if (flag) {
+            span.appendChild(joint.cloneNode(true));
+        }
+        else {
+            flag = true;
+        }
+        span.appendChild(e);
+    }
+    return span;
+}
