6f508870b9c2759563236e9f1144ec469533181a
bmyds1 <78897051+bmyds1@users.noreply.github.com>
2021-03-04 14:41:16 +0900

    Delete main.js

--- pj_sekai/responces/js/main.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", () => {
-    document.getElementById("h1").innerText = document.title;
-    initSelect();
-    initResponcesTable();
-    initFooter();
-    document.getElementById("form").addEventListener("change", initResponcesTable);
-});
